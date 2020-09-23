c2de9f57b91b04a4d9c5228825662f622f7768a1
Vijay Srinivasan <cbevijay@gmail.com>
2020-09-23 16:07:55 +0530

    Add UI Components for listing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailComponent } from './listing-detail/listing-detail.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingsPageComponent
+    ListingsPageComponent,
+    ListingDetailComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingsPageComponent implements OnInit {
 
+  listings: Listing[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeListings;
   }
 
 }
